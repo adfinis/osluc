b4c6b339d7dd9bc00bf705d651c6f99866b4afe8
Ales Zelenik <ales.zelenik@adfinis.com>
2024-11-06 14:57:18 +0100

    fix: delete user only after identity, limit return of attrs,  use debug to output, add filetime conversion

--- osluc_test.go
+++ osluc_test.go
@@ -1,3 +1,22 @@
 package main
 
-// TODO
+import (
+	"testing"
+	"time"
+)
+
+func TestFileTimeToGoTime(t *testing.T) {
+	t.Parallel()
+
+	testString := "133753723630000000"
+	testTime := time.Date(2024, time.November, 6, 13, 12, 43, 0, time.UTC)
+	resultTime, err := FileTimeToGoTime(testString)
+
+	if err != nil {
+		t.Fatalf(`Error %s`, err)
+	}
+
+	if resultTime != testTime {
+		t.Fatalf(`FileTimeToGoTime(%v) = %s, want %s`, nil, resultTime, testTime)
+	}
+}
